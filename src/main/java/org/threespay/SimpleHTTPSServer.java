6ccffb8f5fc9b0ff73071ac333b9476697f9b264
imadev <mebarkiahmedimadeddine@gmail.com>
2023-11-08 01:13:53 +0100

    First commit

---
+++ src/main/java/org/threespay/SimpleHTTPSServer.java
@@ -0,0 +1,92 @@
+package org.threespay;
+
+import com.sun.net.httpserver.*;
+
+import javax.net.ssl.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.security.*;
+import java.security.cert.CertificateException;
+
+public class SimpleHTTPSServer {
+    private static final String SECRET = "password";
+    public static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "This is the response";
+            HttpsExchange httpsExchange = (HttpsExchange) t;
+            t.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            t.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+
+        try {
+            // setup the socket address
+            InetSocketAddress address = new InetSocketAddress(8000);
+
+            // initialise the HTTPS server
+            HttpsServer httpsServer = HttpsServer.create(address, 0);
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+
+            // initialise the keystore
+            char[] password = SECRET.toCharArray();
+            KeyStore ks = KeyStore.getInstance("JKS");
+            InputStream is = SimpleHTTPSServer.class.getClassLoader().getResourceAsStream("testkey.jks"); //WHEN JKS FILE IN RESOURCES
+            FileInputStream fis = new FileInputStream("C:\\Users\\imadev\\Desktop\\3spay\\project\\httpserver\\testkey.jks"); //WHEN JKS FILE NOT IN RESOURCES
+            ks.load(is, password);
+//            ks.load(fis, password);
+
+            // setup the key manager factory
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            // setup the trust manager factory
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            tmf.init(ks);
+
+            // setup the HTTPS context and parameters
+            sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
+            httpsServer.setHttpsConfigurator(new HttpsConfigurator(sslContext) {
+                public void configure(HttpsParameters params) {
+                    try {
+                        // initialise the SSL context
+                        SSLContext context = getSSLContext();
+                        SSLEngine engine = context.createSSLEngine();
+                        params.setNeedClientAuth(false);
+                        params.setCipherSuites(engine.getEnabledCipherSuites());
+                        params.setProtocols(engine.getEnabledProtocols());
+
+                        // Set the SSL parameters
+                        SSLParameters sslParameters = context.getSupportedSSLParameters();
+                        params.setSSLParameters(sslParameters);
+
+                    } catch (Exception ex) {
+                        System.out.println("Failed to create HTTPS port");
+                    }
+                }
+            });
+            httpsServer.createContext("/test", new MyHandler());
+            httpsServer.setExecutor(null); // creates a default executor
+            httpsServer.start();
+            System.out.println("Serveur started");
+        } catch (Exception exception) {
+            System.out.println("Failed to create HTTPS server on port " + 8000 + " of localhost");
+            exception.printStackTrace();
+
+        }
+    }
+
+}
